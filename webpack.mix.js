9e35084b8565c067e2aaa37a9e6d6c39d7cc2bc2
antlogist <antlogist@gmail.com>
2021-07-31 14:25:54 +0300

    Combine CSS and JS file

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,18 @@
-let mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
-mix.sass('resources/assets/sass/app.scss', 'resources/assets/css');
+//Compile SASS in CSS
+mix.sass("resources/assets/sass/app.scss", "resources/assets/css");
+
+//Combine CSS files
+mix.combine([
+  "resources/assets/css/app.css",
+  "resources/assets/bower/vendor/slick-carousel/slick/slick.css"
+], "public/css/all.css");
+
+//Combine JS files
+const bower = "resources/assets/bower/vendor";
+mix.scripts([
+  bower + "/jquery/dist/jquery.min.js",
+  bower + "/foundation-sites/dist/js/foundation.min.js",
+  bower + "/slick-carousel/slick/slick.min.js"
+], "public/js/all.js");
