12b231e895c256432f3364a34dddb39bd46c7aeb
antlogist <antlogist@gmail.com>
2021-08-05 15:33:17 +0300

    Route Dispatcher added

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -1,4 +1,10 @@
 <?php
+
+//Start session if it is not already started
+if(!isset($_SESSION)) session_start();
+
 require_once __DIR__ . "/../app/config/_env.php";
 
-require_once __DIR__ . "/../app/routing/routes.php";
+require_once __DIR__ . "/../app/Routing/routes.php";
+
+new \App\Routing\RouteDispatcher($router);
