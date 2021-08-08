41e6fe65e5ddd773bcaeea984296e71954e80bdc
antlogist <antlogist@gmail.com>
2021-08-08 11:29:15 +0300

    DB connection test

--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
 <?php
 
 require_once __DIR__ . "/../bootstrap/init.php";
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$category = Capsule::select('SELECT * FROM categories');
+
+echo "<pre>";
+var_dump($category);
+echo "</pre>";
