02f1b753ef90af574cc36e293f75eabc2704977b
antlogist <antlogist@gmail.com>
2021-08-10 13:00:45 +0300

    Mail class added

---
+++ app/Classes/Mail.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Classes;
+
+use PHPMailer;
+	
+class Mail {
+	protected $mail;
+	
+	function __construct() {
+		$this->mail = new PHPMiler();
+		$this->setUp();
+	}
+	
+	function setUp() {
+		$this->mail->isSMTP();
+		$this->mail->Mailer = "smtp";
+		$this->mail->SMTPAuth = true;
+		$this->mail->SMTPSecure = "tls";
+		
+		$this->mail->Host = $_SERVER["SMTP_HOST"];
+		$this->mail->Port = $_SERVER["SMTP_PORT"];
+		
+		$environment = $_SERVER["APP_ENV"];
+		
+		if ($environment === "local") {
+			$this->mail->SMTPDebug = 2;
+		}
+		
+		//auth info
+		$this->mail->Username = $_SERVER["EMAIL_USERNAME"];
+		$this->mail->Password = $_SERVER["EMAIL_PASSWORD"];
+		
+		$this->mail->isHTML(true);
+		$this->mail->SingleTo = true;
+		
+		//sender info
+		$this->mail->From = $_SERVER("ADMIN_EMAIL");
+		$this->mail->FromName = "MVC_Store";
+	}
+	
+	function send($data) {
+		$this->mail->addAddress($data["to"], $data["name"]);
+		$this->mail->Subject = $data["subject"];
+		$this->mail->Body = "";
+	}
+	
+}
