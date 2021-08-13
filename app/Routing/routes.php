c5eff7ad5635c124bb10ab46b66471dabd70470b
antlogist <antlogist@gmail.com>
2021-08-13 06:25:42 +0300

    Blade fixed

--- app/Routing/routes.php
+++ app/Routing/routes.php
@@ -3,3 +3,6 @@
 $router = new AltoRouter();
 
 $router->map("GET", "/mvc/", "App\Controllers\IndexController@show", "home");
+
+//Admin routs
+$router->map("GET", "/mvc/admin", "App\Controllers\Admin\DashboardController@show", "admin_dashboard");
