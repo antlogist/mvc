bded39dd717374a7e3187aff87573bc4ec2f7013
antlogist <antlogist@gmail.com>
2021-08-15 08:14:09 +0300

    Function _token added

--- app/Classes/CSRFToken.php
+++ app/Classes/CSRFToken.php
@@ -2,6 +2,19 @@
 
 namespace App\Classes;
 
+use App\Classes\Session;
+
 class CSRFToken {
+  static function _token() {
+      if (!Session::has("token")) {
+        $randomToken = base64_decode(openssl_random_pseudo_bytes(32));
+        Session::add("token", $randomToken);
+      }
+    
+    return Session::get("token");
+  }
   
+  static function verifyCSRFToken($requestToken) {
+    
+  }
 }
