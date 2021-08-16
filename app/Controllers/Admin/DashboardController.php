36120520fa46f8d559c24accb9a19f813e78f33d
antlogist <antlogist@gmail.com>
2021-08-16 09:11:26 +0300

    Markup fixed

--- app/Controllers/Admin/DashboardController.php
+++ app/Controllers/Admin/DashboardController.php
@@ -7,17 +7,19 @@ use App\Classes\Request;
 use App\Controllers\BaseController;
 
 class DashboardController extends BaseController {
-	function show() {
-      Session::add("admin", "You are welcome!");
-      
-      if (Session::has("admin")) {
-        $msg = Session::get("admin");
-      } else {
-        $msg = "Not defined";
-      }
-      
-      return view("admin/dashboard", ["admin" => $msg]);
-	}
+
+  function show() {
+    Session::add("admin", "You are welcome!");
+
+    if (Session::has("admin")) {
+      $msg = Session::get("admin");
+    } else {
+      $msg = "Not defined";
+    }
+
+    return view("admin/dashboard", ["admin" => $msg]);
+  }
+  
   function get() {
     Request::refresh();
     $data = Request::old("post", "product");
