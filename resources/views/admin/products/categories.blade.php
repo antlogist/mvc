994357cd85a4a0cfd8530a23ffd395de7dd8c673
antlogist <antlogist@gmail.com>
2021-08-17 21:25:41 +0300

    Markup fixed, forms added

--- resources/views/admin/products/categories.blade.php
+++ resources/views/admin/products/categories.blade.php
@@ -3,31 +3,56 @@
 
 
 @section("content")
-	<div class="dashboard expanded">
-		<h2>Product Categories</h2>
-		{{ $categories }}
-		<div class="grid-x expanded">
-		  <div class="small-12 medium-6 cell">
-		    <form action="" method="post">
-		      <div class="input-group">
-		        <input type="text" class="input-group-field" placeholder="Search by name">
-		        <div class="input-group-button">
-		          <input type="button" class="button" value="Search">
-		        </div>
-		      </div>
-		    </form>
-		  </div>
-		  
-		  <div class="small-12 medium-6 cell">
-		    <form action="./admin/products/categories" method="post">
-		      <div class="input-group">
-		        <input type="text" name="name" class="input-group-field" placeholder="Category name">
-		        <div class="input-group-button">
-		          <input type="button" class="button" value="Search">
-		        </div>
-		      </div>
-		    </form>
-		  </div>
-		</div>
-	</div>
+<div class="dashboard expanded grid-container">
+  <h2>Product Categories</h2>
+  {{ $categories }}
+  <div class="grid-x grid-margin-x expanded">
+    <div class="small-12 medium-6 cell">
+      <form action="" method="post">
+        <div class="input-group">
+          <input type="text" class="input-group-field" placeholder="Search by name">
+          <div class="input-group-button">
+            <input type="button" class="button" value="Search">
+          </div>
+        </div>
+      </form>
+    </div>
+
+    <div class="small-12 medium-6 cell">
+      <form action="./admin/products/categories" method="post">
+        <div class="input-group">
+          <input type="text" name="name" class="input-group-field" placeholder="Category name">
+          <input type="hidden" name="token" value="{{ \App\Classes\CSRFToken::_token() }}">
+          <div class="input-group-button">
+            <input type="button" class="button" value="Create">
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <div class="grid-x grid-margin-x expanded">
+    <div class="small-12 medium-12 cell">
+      @if(count($categories))
+      <table class="hover">
+        <tbody>
+          @foreach($categories as $category)
+          <tr>
+            <td>{{ $category->name }}</td>
+            <td>{{ $category->slug }}</td>
+            <td>{{ $category->created_at->toFormattedDateString() }}</td>
+            <td width="100" class="text-right">
+              <a href="#"><i class="fa fa-edit"></i></a>
+              <a href="#"><i class="fa fa-times"></i></a>
+            </td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+      @else
+      <h3>You haven't any category.</h3>
+      @endif
+    </div>
+  </div>
+</div>
 @endsection
