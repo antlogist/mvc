1534f1dbb3474bcd573bbb22232aba60d6459ee7
antlogist <antlogist@gmail.com>
2021-08-19 14:20:52 +0300

    Validation functions set as protected and comments added

--- app/Classes/ValidateRequest.php
+++ app/Classes/ValidateRequest.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 class ValidateRequest {
 
+  /**
+   * [[Description]]
+   * @param $column, field name or column
+   * @param $value, value passed into the form
+   * @param $policy, the rule we set
+   * @return boolean, true | false
+   */
   protected static function unique($column, $value, $policy) {
     if ($value !== null && !empty(trim($value))) {
       //return false if value already exists
@@ -14,46 +21,46 @@ class ValidateRequest {
     return true;
   }
   
-  static function required($column, $value, $policy) {
+  protected static function required($column, $value, $policy) {
     return $value !== null && !empty(trim($value));
   }
   
-  static function minLength($column, $value, $policy) {
+  protected static function minLength($column, $value, $policy) {
      if ($value !== null && !empty(trim($value))) {
        return strlen($value) >= $policy;
      }
     return true;
   }
   
-  static function maxLength($column, $value, $policy) {
+  protected static function maxLength($column, $value, $policy) {
      if ($value !== null && !empty(trim($value))) {
        return strlen($value) <= $policy;
      }
     return true;
   }
   
-  static function email($column, $value, $policy) {
+  protected static function email($column, $value, $policy) {
      if ($value !== null && !empty(trim($value))) {
        return filter_var($value, FILTER_VALIDATE_EMAIL);
      }
     return true;
   }
   
-  static function mixed($column, $value, $policy) {
+  protected static function mixed($column, $value, $policy) {
      if (!preg_match("/^[a-zA-Z0-9 .,_~\-!@#\&%\^\'\*\(\)]*$/", $value)) {
        return false;
      }
     return true;
   }
   
-  static function string($column, $value, $policy) {
+  protected static function string($column, $value, $policy) {
      if (!preg_match("/^[a-zA-Z ]*$/", $value)) {
        return false;
      }
     return true;
   }
   
-  static function numbers($column, $value, $policy) {
+  protected static function numbers($column, $value, $policy) {
      if (!preg_match("/^[0-9. ]*$/", $value)) {
        return false;
      }
