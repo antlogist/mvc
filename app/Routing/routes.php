9ba5df1fe3fac5a2218bcb34bb90dc1bda333e5e
antlogist <antlogist@gmail.com>
2021-08-25 14:38:19 +0300

    Routes separated into two files

--- app/Routing/routes.php
+++ app/Routing/routes.php
@@ -5,16 +5,4 @@ $router = new AltoRouter();
 $router->map("GET", "/mvc/", "App\Controllers\IndexController@show", "home");
 
 //Admin routs
-$router->map("GET", "/mvc/admin", "App\Controllers\Admin\DashboardController@show", "admin_dashboard");
-$router->map("POST", "/mvc/admin", "App\Controllers\Admin\DashboardController@get", "admin_form");
-
-//Product management
-$router->map("GET", "/mvc/admin/product/categories", 
-             "App\Controllers\Admin\ProductCategoryController@show", "product_category");
-$router->map("POST", "/mvc/admin/product/categories", 
-             "App\Controllers\Admin\ProductCategoryController@store", "create_product_category");
-
-$router->map("POST", "/mvc/admin/product/categories/[i:id]/edit", 
-             "App\Controllers\Admin\ProductCategoryController@edit", "edit_product_category");
-$router->map("POST", "/mvc/admin/product/categories/[i:id]/delete", 
-             "App\Controllers\Admin\ProductCategoryController@delete", "delete_product_category");
+require_once __DIR__ . "/admin_routes.php";
