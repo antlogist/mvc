3709c10d274ee9d58e9b8fe73b115eeacc0c36b2
antlogist <antlogist@gmail.com>
2021-08-27 16:15:05 +0300

    Update subcatigory function added

--- public/js/all.js
+++ public/js/all.js
@@ -102,7 +102,46 @@
             $(".notification").css("display", "block").removeClass("primary", "success").addClass("alert").delay(6000).slideUp(300).html(ul);
           }
       });
-    })
+    });
+    
+    // update subcategory
+    $(".update-subcategory").on("click", function (e) {
+      e.preventDefault();
+      const token = $(this).data("token");
+      const id = $(this).attr("id");
+      const name = $("#item-subcategory-name-" + id).val();
+      let category_id = $(this).data("category-id");
+      const selected_category_id = $("#item-category-" + category_id + " option:selected").val();
+      
+      if (category_id !== selected_category_id) {
+        category_id = selected_category_id;
+      }
+      
+      $.ajax({
+        type: "POST",
+        url: `/mvc/admin/product/subcategory/${id}/edit`,
+        data: {
+          token: token,
+          name: name,
+          category_id: category_id
+        },
+          success: function (data) {
+            const response = jQuery.parseJSON(data);
+            $(".notification").css("display", "block").removeClass("primary", "alert").addClass("success").delay(4000).slideUp(300).html(response.success);
+          },
+          error: function (request, error) {
+            const errors = jQuery.parseJSON(request.responseText);
+            const ul = document.createElement("ul");
+            $.each(errors, function (key, value) {
+              const li = document.createElement("li");
+              li.appendChild(document.createTextNode(value));
+              ul.appendChild(li);
+            })
+            $(".notification").css("display", "block").removeClass("primary", "success").addClass("alert").delay(6000).slideUp(300).html(ul);
+          }
+      });
+    });
+    
 
   };
 })();
