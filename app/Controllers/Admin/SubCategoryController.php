dc00731f47ce44dd13250130dfef859ca3765455
antlogist <antlogist@gmail.com>
2021-08-27 16:10:39 +0300

    Edit function added

--- app/Controllers/Admin/SubCategoryController.php
+++ app/Controllers/Admin/SubCategoryController.php
@@ -65,4 +65,58 @@ class SubCategoryController extends BaseController {
     }
     return null;
   }
+  
+  function edit($id) {
+    
+    if (Request::has("post")) {
+      $request = Request::get("post");
+      
+      $extra_errors = [];
+      
+      //Token validation
+      if (CSRFToken::verifyCSRFToken($request->token, false)) {
+        
+        //Cat name validation process
+        $validate = new ValidateRequest;
+        
+        
+        //Subcats duplicateion validation
+        $duplicate_subcategory = SubCategory::where("name", $request->name)
+                    ->where("category_id", $request->category_id)->exists();
+        
+        if ($duplicate_subcategory) {
+          $extra_errors["name"] = array("Ypu have not make any changes");
+        }
+        
+        //If cat does not exist
+        $category = Category::where("id", $request->category_id)->exists();
+        if (!$category) {
+          $extra_errors["name"] = array("Invalid product category");
+        }
+        
+        //If has errors
+        if ($validate->hasError() || $duplicate_subcategory || !$category) {
+          $errors = $validate->getErrorMessages();
+          count($extra_errors) ? $response = array_merge($errors, $extra_errors) : $response = $errors;
+          
+          header("HTTP/1.1 422 Unprocessible Entity", true, 422);
+          echo json_encode($response);
+          exit;
+        }
+
+        //Process form data
+        SubCategory::where("id", $id)->update([
+          "name" => $request->name,
+          "category_id" => $request->category_id,
+        ]);
+        echo json_encode([
+          "success" => "Subcategory updated successfully"
+        ]);
+        exit;
+      }
+      throw new \Exception("Token mismatch");
+    }
+    return null;
+  }
+  
 }
