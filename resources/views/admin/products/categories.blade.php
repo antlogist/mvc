0be3124a8600453592be03fffb8f592fd74a2250
antlogist <antlogist@gmail.com>
2021-08-27 14:09:50 +0300

    Subcatigory table added to the blade template

--- resources/views/admin/products/categories.blade.php
+++ resources/views/admin/products/categories.blade.php
@@ -133,5 +133,74 @@
     
   </div>
 </div>
+ 
+<div class="dashboard expanded grid-container">
+  <h3>Sub categories</h3>
+  <div class="grid-x grid-margin-x expanded">
+    <div class="small-12 medium-12 cell">
+     
+      @if(count($subcategories))
+      <table class="hover" data-form="deleteForm">
+        <tbody>
+          @foreach($subcategories as $subcategory)
+          <tr>
+            <td>{{ $subcategory["name"] }}</td>
+            <td>{{ $subcategory["slug"] }}</td>
+            <td>{{ $subcategory["added"] }}</td>
+            
+            <!--Buttons-->
+            <td width="100" class="text-right">
+
+              <!--Edit button-->
+              <span data-tooltip tabindex="1" title="Edit Subategory">
+                <a href="#" data-open="item-subcategory-{{ $subcategory['id'] }}"><i class="fa fa-edit"></i></a>
+              </span>
+              <!--/Edit button-->
+
+              <!--Delete button-->
+              <span data-tooltip tabindex="1" title="Delete Subcategory" style="display: inline-block">
+                <form method="POST" action="<?php echo $_SERVER["APP_URL"] ?>/admin/product/subcategory/{{ $subcategory['id'] }}/delete" class="delete-item">
+                  <input type="hidden" name="token" value="{{ \App\Classes\CSRFToken::_token() }}">
+                  <button type="submit"><i class="fa fa-times"></i></button>
+                </form>
+              </span>
+              <!--/Delete button-->
+              
+              
+              <!--Edit Subcategory Modal-->
+              <div class="reveal" id="item-subcategory-{{ $subcategory['id'] }}" data-reveal data-close-on-click="false" data-animation-in="scale-in-up" data-animation-out="scale-out-down">
+                <div class="notification callout primary"></div>
+                <h2>Edit subcategory</h2>
+                <!--Form-->
+                <form>
+                  <div class="input-group">
+                    <input id="item-subcategory-name-{{ $subcategory['id'] }}" type="text" value="{{ $subcategory['name'] }}">
+                    <div>
+                      <input type="submit" class="button update-subcategory" id="{{ $subcategory['id'] }}" data-token="{{ \App\Classes\CSRFToken::_token() }}" value="Update">
+                    </div>
+                  </div>
+                </form>
+                <!--/Form-->
+                <a href="<?php echo $_SERVER["APP_URL"] ?>/admin/product/categories" class="close-button" data-close aria-label="Close modal" type="button">
+                  <span aria-hidden="true">&times;</span>
+                </a>
+              </div>
+              <!--/Edit Subcategory Modal-->
+              
+            </td>
+            
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+      
+      {!! $subcategories_links !!}
+      
+      @else
+      <h3>You haven't any subcategory.</h3>
+      @endif
+    </div>
+  </div>
+</div>
 @include("includes.delete-modal")
 @endsection
