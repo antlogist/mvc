4afb8334a37fb9fe51e58a5deb19bb81da977273
antlogist <antlogist@gmail.com>
2021-08-30 12:09:40 +0300

    Route for the product creation form update

--- app/Routing/admin_routes.php
+++ app/Routing/admin_routes.php
@@ -24,5 +24,7 @@ $router->map("POST", "/mvc/admin/product/subcategory/[i:id]/delete",
              "App\Controllers\Admin\SubCategoryController@delete", "delete_product_subcategory");
 
 //Products
+$router->map("GET", "/mvc/admin/product/product/create", 
+             "App\Controllers\Admin\ProductController@showCreateProductForm", "create_product_form");
 $router->map("POST", "/mvc/admin/product/product/create", 
              "App\Controllers\Admin\ProductController@store", "create_product");
