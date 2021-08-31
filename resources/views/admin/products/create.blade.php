4f9a219fae85f804dd25630e4c63c81923960731
antlogist <antlogist@gmail.com>
2021-08-31 08:54:59 +0300

    Comments added

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -11,18 +11,24 @@
 
   <form method="post" action="<?php echo $_SERVER["APP_URL"] ?>/admin/product/create">
     <div class="grid-x grid-margin-x expanded">
+
+      <!--Product name - text input-->
       <div class="small-12 medium-6 cell">
         <label>Product name:
           <input type="text" name="name" placeholder="Product name" value="{{ \App\Classes\Request::old('post', 'name') }}">
         </label>
       </div>
+      <!--/Product name - text input-->
 
+      <!--Product price - text input-->
       <div class="small-12 medium-6 cell">
         <label>Product price:
           <input type="text" name="price" placeholder="Product price" value="{{ \App\Classes\Request::old('post', 'price') }}">
         </label>
       </div>
+      <!--/Product price - text input-->
 
+      <!--Product cat - select-->
       <div class="small-12 medium-6 cell">
         <label>Product Category:
           <select name="category" id="product-category">
@@ -35,7 +41,9 @@
           </select>
         </label>
       </div>
+      <!--/Product cat - select-->
 
+      <!--Product subcat - select-->
       <div class="small-12 medium-6 cell">
         <label>Product Subcategory:
           <select name="subcategory" id="product-subcategory">
@@ -45,7 +53,9 @@
           </select>
         </label>
       </div>
+      <!--/Product subcat - select-->
 
+      <!--Product quantity - select-->
       <div class="small-12 medium-6 cell">
         <label>Product Quantity:
           <select name="quantity">
@@ -58,14 +68,18 @@
           </select>
         </label>
       </div>
+      <!--/Product quantity - select-->
 
+      <!--Product image - input file-->
       <div class="small-12 medium-6 cell">
         <br />
         <div class="input-group"><span class="input-group-label">Product Image:</span>
           <input type="file" name="productImage" class="input-group-field">
         </div>
       </div>
+      <!--/Product image - input file-->
 
+      <!--Product description - textarea-->
       <div class="small-12 cell">
         <label>Description:
           <textarea name="description" placeholder="Description">{{\App\Classes\Request::old('post', 'description')}}</textarea>
@@ -74,6 +88,7 @@
         <button class="button alert" type="reset">Reset</button>
         <input class="button success float-right" type="submit" value="Save Product">
       </div>
+      <!--/Product description - textarea-->
 
     </div>
   </form>
