e2f638c0577b39fc03f82b9a4bfb72c6e69a5d06
antlogist <antlogist@gmail.com>
2021-09-01 21:43:30 +0300

    function edit added

--- app/Controllers/Admin/ProductController.php
+++ app/Controllers/Admin/ProductController.php
@@ -123,4 +123,72 @@ class ProductController extends BaseController {
     }
     return null;
   }
+  
+  function edit() {
+    if (Request::has("post")) {
+      $request = Request::get("post");
+      //Token validation
+      if (CSRFToken::verifyCSRFToken($request->token, false)) {
+        //Validation rules
+        $rules = [
+          "name" => ["required" => true, "minLength" => 3, "maxLength" => 70, "string" => true],
+          "price" => ["required" => true, "minLength" => 2, "numbers" => true],
+          "quantity" => ["required" => true],
+          "category" => ["required" => true],
+          "sub_category" => ["required" => true],
+          "description" => ["required" => true, "mixed" => true, "minLength" => 4, "maxLength" => 500],
+        ];
+        //Product name validation process
+        $validate = new ValidateRequest;
+        $validate->abide($_POST, $rules);
+        
+        //File validation
+        $file = Request::get("file");
+        isset($file->productImage->name) ? $filename = $file->productImage->name : $filename = "";
+        
+        $file_error = [];
+        
+        if (isset($file->productImage->name) && !UploadFile::isImage($filename)){
+          $file_error["productImage"] = ["The file is not an image, please try again!"];
+        }
+        
+        //If has errors
+        if ($validate->hasError() || count($file_error)) {
+          $response = $validate->getErrorMessages();
+          count($file_error) ? $errors = array_merge($response, $file_error) : $errors = $response;
+          return view("admin/products/create", [
+            "categories" => $this->categories,
+            "errors" => $errors
+          ]);
+        }
+        
+        $product = Product::findOrFail($request->product_id);
+        
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->quantity = $request->quantity;
+        $product->category_id = $request->category;
+        $product->sub_category_id = $request->subcategory;
+        
+        if ($filename) {
+          //Directory separator
+          $ds = DIRECTORY_SEPARATOR;
+          $old_image_path = BASE_PATH."{$ds}public{$ds}$product->image_path";
+          $temp_file = $file->productImage->tmp_name;
+          $image_path = UploadFile::move($temp_file, "images{$ds}uploads{$ds}products", $filename)->path();
+          unlink($old_image_path);
+          $product->image_path = $image_path;
+        }
+        
+        $product->save();
+        
+        Session::add("success", "Record Updated");
+        Redirect::to($_SERVER["APP_URL"] . "/admin/products");
+        exit;
+      }
+      throw new \Exception("Token mismatch");
+    }
+    return null;
+  }
 }
