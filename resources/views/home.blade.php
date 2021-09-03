99a4ddd5e7a52c877f7faab0049edd322690862d
antlogist <antlogist@gmail.com>
2021-09-03 08:42:56 +0300

    Home template created

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends("layouts.app");
+
+@section("title", "Home Page")
+
+@section("data-page-id", "home")
+
+@section("content")
+
+<div class="grid-x grid-margin-x expanded">
+  
+  <h1>Homepage</h1>
+  
+</div>
+
+@stop
