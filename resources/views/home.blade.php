2247a791e8bcf988090dd4d34b10ec9be411d683
antlogist <antlogist@gmail.com>
2021-09-03 14:24:24 +0300

    Slider added to thehome page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,23 +5,18 @@
 @section("data-page-id", "home")
 
 @section("content")
-<div class="grid-container">
-  <div class="grid-x grid-margin-x">
+<div class="home">
+  <div class="hero-slider">
+    <div> <img src="/mvc/public/images/sliders/test.jpg" alt="MVC Store"> </div>
+    <div> <img src="/mvc/public/images/sliders/test.jpg" alt="MVC Store"> </div>
+    <div> <img src="/mvc/public/images/sliders/test.jpg" alt="MVC Store"> </div>
+    <div class="grid-container">
+      <div class="grid-x grid-margin-x">
 
-    <h1>Homepage</h1>
-
-    <div class="home">
-
-      <section class="hero">
-        <div class="hero-slider">
-          <div> <img src="mvc/oublic/images/sliders/test.jpg" alt="Acme Store"> </div>
-          <div> <img src="mvc/oublic/images/sliders/test.jpg" alt="Acme Store"> </div>
-          <div> <img src="mvc/oublic/images/sliders/test.jpg" alt="Acme Store"> </div>
-        </div>
-      </section>
+      </div>
     </div>
-
   </div>
+
 </div>
 
 @stop
