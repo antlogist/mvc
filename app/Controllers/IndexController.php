a89e91a8b271cbb50cb2a5641343db3c3e45cf35
antlogist <antlogist@gmail.com>
2021-09-03 08:47:59 +0300

    Home view added

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -4,23 +4,7 @@ namespace App\Controllers;
 use App\Classes\Mail;
   
 class IndexController extends BaseController {
-	public function show() {
-		echo "Inside Homepage from controller class";
-		
-		$mail = new Mail();
-	  	
-		$data = [
-			"to" => "test@test.com",
-			"subject" => "Welcome to MVC store example",
-			"view" => "welcome",
-			"name" => "Anthony Underwood",
-			"body" => "Email template"
-		];
-
-		if($mail->send($data)) {
-			echo "Email send successfully!";
-		} else {
-			echo "Email sending failed!";
-		}
-	}
+	function show() {
+      return view("home");
+    }
 }
