f64bdb0c313f3798deaae5db0c51fbeef90199de
antlogist <antlogist@gmail.com>
2021-09-03 09:12:52 +0300

    Grid structure fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,12 @@
 @section("data-page-id", "home")
 
 @section("content")
+<div class="grid-container">
+  <div class="grid-x grid-margin-x">
 
-<div class="grid-x grid-margin-x expanded">
-  
-  <h1>Homepage</h1>
-  
+    <h1>Homepage</h1>
+
+  </div>
 </div>
 
 @stop
