9b2203d25678e8cc0ab58b63b05e0b2e9d451b60
antlogist <antlogist@gmail.com>
2021-09-03 11:40:34 +0300

    Slider markup added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,16 +9,16 @@
   <div class="grid-x grid-margin-x">
 
     <h1>Homepage</h1>
-    
+
     <div class="home">
-        
-        <section class="hero">
-            <div class="hero-slider">
-                <div> <img src="mvc/oublic/images/sliders/test.jpg" alt="Acme Store"> </div>
-                <div> <img src="mvc/oublic/images/sliders/test.jpg" alt="Acme Store"> </div>
-                <div> <img src="mvc/oublic/images/sliders/test.jpg" alt="Acme Store"> </div>
-            </div>
-        </section>
+
+      <section class="hero">
+        <div class="hero-slider">
+          <div> <img src="mvc/oublic/images/sliders/test.jpg" alt="Acme Store"> </div>
+          <div> <img src="mvc/oublic/images/sliders/test.jpg" alt="Acme Store"> </div>
+          <div> <img src="mvc/oublic/images/sliders/test.jpg" alt="Acme Store"> </div>
+        </div>
+      </section>
     </div>
 
   </div>
