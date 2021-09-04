f1c7e7976daed68ffa1b768c8360dacb3e299701
antlogist <antlogist@gmail.com>
2021-09-04 15:42:30 +0300

    Route for featured products created

--- app/Routing/routes.php
+++ app/Routing/routes.php
@@ -3,6 +3,7 @@
 $router = new AltoRouter();
 
 $router->map("GET", "/mvc/", "App\Controllers\IndexController@show", "home");
+$router->map("GET", "/mvc/featured", "App\Controllers\IndexController@featuredProducts", "feature_product");
 
 //Admin routs
 require_once __DIR__ . "/admin_routes.php";
