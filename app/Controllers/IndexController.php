6d13158761c085aa01f90ebf827f440f95fcb9ed
antlogist <antlogist@gmail.com>
2021-09-05 21:27:09 +0300

    function featured products added

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -1,10 +1,15 @@
 <?php
 namespace App\Controllers;
 
-use App\Classes\Mail;
+use App\Models\Product;
   
 class IndexController extends BaseController {
 	function show() {
       return view("home");
     }
+  
+  function featuredProducts() {
+    $products = Product::where("featured", 1)->inRandomOrder()->limit(4)->get();
+    echo json_encode(["featured" => $products]);
+  }
 }
