abbeeced70d56bf7e27fbe37f0addec7d50fd98c
antlogist <antlogist@gmail.com>
2021-09-08 08:40:31 +0300

    POST request for load more products added

--- app/Routing/routes.php
+++ app/Routing/routes.php
@@ -5,6 +5,7 @@ $router = new AltoRouter();
 $router->map("GET", "/mvc/", "App\Controllers\IndexController@show", "home");
 $router->map("GET", "/mvc/featured", "App\Controllers\IndexController@featuredProducts", "feature_product");
 $router->map("GET", "/mvc/get-products", "App\Controllers\IndexController@getProducts", "get_product");
+$router->map("GET", "/mvc/load-more", "App\Controllers\IndexController@loadMoreProducts", "load_more_product");
 
 //Admin routs
 require_once __DIR__ . "/admin_routes.php";
