8747a192ea782a869bda5fffbd8dda27c75997bf
antlogist <antlogist@gmail.com>
2021-09-09 22:49:54 +0300

    product details added

--- public/js/all.js
+++ public/js/all.js
@@ -241,10 +241,10 @@
   }
 })();
 
-(function() {
+(function () {
   "use strict";
-  
-  MVCSTORE.product.details = function() {
+
+  MVCSTORE.product.details = function () {
     const app = new Vue({
       el: "#product",
       data: {
@@ -253,11 +253,35 @@
         subCategory: [],
         productId: $("#product").data("id"),
         loading: false
+      },
+      methods: {
+        getProductDetails: function () {
+          this.loading = true;
+          setTimeout(function () {
+            axios.get("/mvc/product-details/" + app.productId).then(function (response) {
+              app.product = response.data.product;
+              app.category = response.data.category;
+              app.subCategory = response.data.subCategory;
+              app.loading = false;
+            });
+          }, 1000)
+        },
+        stringLimit: function (string, value) {
+          if (string.length > value) {
+            return string.substring(0, value) + '...';
+          } else {
+            return string;
+          }
+        },
+      },
+      created: function () {
+        this.getProductDetails();
       }
     });
   }
-  
+
 })();
+
 (function () {
     "use strict";
 
