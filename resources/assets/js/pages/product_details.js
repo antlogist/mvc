efa6dff186061b606c9dffe64cc0a0637129e5cf
antlogist <antlogist@gmail.com>
2021-09-09 22:47:57 +0300

    getProductDetails added

--- resources/assets/js/pages/product_details.js
+++ resources/assets/js/pages/product_details.js
@@ -1,7 +1,7 @@
-(function() {
+(function () {
   "use strict";
-  
-  MVCSTORE.product.details = function() {
+
+  MVCSTORE.product.details = function () {
     const app = new Vue({
       el: "#product",
       data: {
@@ -10,8 +10,31 @@
         subCategory: [],
         productId: $("#product").data("id"),
         loading: false
+      },
+      methods: {
+        getProductDetails: function () {
+          this.loading = true;
+          setTimeout(function () {
+            axios.get("/mvc/product-details/" + app.productId).then(function (response) {
+              app.product = response.data.product;
+              app.category = response.data.category;
+              app.subCategory = response.data.subCategory;
+              app.loading = false;
+            });
+          }, 1000)
+        },
+        stringLimit: function (string, value) {
+          if (string.length > value) {
+            return string.substring(0, value) + '...';
+          } else {
+            return string;
+          }
+        },
+      },
+      created: function () {
+        this.getProductDetails();
       }
     });
   }
-  
+
 })();
