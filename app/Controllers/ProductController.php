5a35bb64808bbc172414bd6fb2e320148d609811
antlogist <antlogist@gmail.com>
2021-09-09 11:14:22 +0300

    Get function added

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -11,4 +11,19 @@ class ProductController extends BaseController {
       $product = Product::where("id", $id)->first();
       return view("product", compact("token", "product"));
     }
+  
+    function get($id) {
+      $product = Product::where("id", $id)->with(["category", "subCategory"])->first();
+      if($product) {
+        echo json_encode([
+          "product" => $product,
+          "category" => $product->category,
+          "subCategory" => $product->subCategory,
+        ]);
+          exit;
+      }
+      header("HTTP/1.1 422 Unprocessable Entity", true, 422);
+      echo "Product not found";
+      exit;
+    }
 }
