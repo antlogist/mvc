2fd041b5e3ba412cfc866fdd34cecd478fe4df08
antlogist <antlogist@gmail.com>
2021-09-15 21:01:48 +0300

    Styles and script versions generation added. I want to avoid cace ^^

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <title>MVC Store - @yield("title")</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="<?php echo $_SERVER["APP_URL"] ?>/public/css/all.css">
+  <link rel="stylesheet" href="<?php echo $_SERVER["APP_URL"] ?>/public/css/all.css<?php echo '?' . mt_rand(); ?>">
   <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
   <script src="https://use.fontawesome.com/bd91b9f588.js"></script>
 </head>
@@ -16,7 +16,7 @@
   @yield("body")
 
 
-  <script src="<?php echo $_SERVER["APP_URL"] ?>/public/js/all.js"></script>
+  <script src="<?php echo $_SERVER["APP_URL"] ?>/public/js/all.js<?php echo '?' . mt_rand(); ?>"></script>
 </body>
 
 </html>
