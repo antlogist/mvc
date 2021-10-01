d2c8124adb779d18142db33e0d7de289cf7965e1
antlogist <antlogist@gmail.com>
2021-10-01 21:15:10 +0300

    Show cart route created

--- app/Routing/routes.php
+++ app/Routing/routes.php
@@ -11,6 +11,7 @@ $router->map("GET", "/mvc/product/[i:id]", "App\Controllers\ProductController@sh
 $router->map("GET", "/mvc/product-details/[i:id]", "App\Controllers\ProductController@get", "product_details");
 
 $router->map("POST", "/mvc/cart", "App\Controllers\CartController@addItem", "add_cart_item");
+$router->map("GET", "/mvc/cart", "App\Controllers\CartController@show", "view_cart");
 
 //Admin routs
 require_once __DIR__ . "/admin_routes.php";
