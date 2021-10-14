b119564c7f648563f361c1cdc95705ae46bfc628
antlogist <antlogist@gmail.com>
2021-10-14 08:47:03 +0300

    Display cart items function added

--- public/js/all.js
+++ public/js/all.js
@@ -193,6 +193,27 @@
         loading: false,
         fail: false,
         message: ""
+      },
+      methods: {
+        displayItems() {
+          this.loading = true;
+          setTimeout(function() {
+            axios.get("/mvc/cart/items").then(function(response) {
+              if(response.data.fail) {
+                app.fail = true;
+                app.message = response.data.fail;
+                app.loading = false;
+              } else {
+                app.items = response.data.items;
+                app.cartTotal = response.data.cartTotal;
+                app.loading = false;
+              }
+            })
+          }, 2000);
+        }
+      },
+      created() {
+        this.displayItems();
       }
     });
   };
