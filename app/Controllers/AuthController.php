5f4d87dfed9c9d637d7f102e8853417c535b0f45
antlogist <antlogist@gmail.com>
2021-10-17 10:53:11 +0300

    isAuthenticated and logout added

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -9,6 +9,12 @@ use App\Classes\ValidateRequest;
 use App\Models\User;
 
 class AuthController extends BaseController {
+  public function __construct() {
+      if(isAuthenticated()) {
+          Redirect::to("/mvc/");
+      }
+  }
+
 	function showRegisterForm() {
     return view("register");
   }
@@ -108,4 +114,18 @@ class AuthController extends BaseController {
     return null;
 
   }
+
+  function logout() {
+      if(isAuthenticated()){
+          Session::remove('SESSION_USER_ID');
+          Session::remove('SESSION_USER_NAME');
+
+          if(!Session::has('user_cart')){
+              session_destroy();
+              session_regenerate_id(true);
+          }
+      }
+      Redirect::to('/mvc/');
+  }
+
 }
