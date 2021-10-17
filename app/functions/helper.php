bf2fffc62c9d5f66359cb00a8709c44acba21e44
antlogist <antlogist@gmail.com>
2021-10-17 10:16:38 +0300

    SessisAuthenticated function added

--- app/functions/helper.php
+++ app/functions/helper.php
@@ -2,36 +2,37 @@
 
 use Philo\Blade\Blade;
 use voku\helper\Paginator;
+use App\Classes\Session;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 function view($path, array $data = []) {
-  
+
   $views = __DIR__ . '/../../resources/views';
   $cache = __DIR__ . '/../../bootstrap/cache';
-  
+
   $blade = new Blade($views, $cache);
-  
+
   echo $blade->view()->make($path, $data)->render();
-  
+
 }
 
 function make($filename, $data) {
-	
+
 	//array keys as variable names and values as variable values
 	extract($data);
-	
+
 	//turn on buffering
 	ob_start();
-	
+
 	//include template
 	include (__DIR__ . "/../../resources/views/emails/" . $filename . ".php");
-	
+
 	//get content of the file
 	$content = ob_get_contents();
-	
+
 	//erase and turn off buffering
 	ob_end_clean();
-	
+
 	return $content;
 }
 
@@ -50,4 +51,8 @@ function paginate($num_of_records, $total_records, $table_name, $object) {
   $data = Capsule::select("SELECT * FROM $table_name WHERE deleted_at is null ORDER BY created_at DESC " . $pages->get_limit());
   $categories = $object->transform($data);
   return [$categories, $pages->page_links()];
+}
+
+function isAuthenticated() {
+    return Session::has("SESSION_USER_NAME") ? true : false;
 }
