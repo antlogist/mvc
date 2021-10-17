03de8aef879a8cf30a988e91588ad46aecb153e7
antlogist <antlogist@gmail.com>
2021-10-17 07:53:55 +0300

    User model created

---
+++ app/Models/User.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Carbon\Carbon;
+use App\Models\Product;
+use App\Models\SubCategory;
+
+class User extends Model {
+
+  use SoftDeletes;
+
+  public $timestamps = true;
+  protected $fillable = ['username', 'fullname', 'email', 'password', 'address', 'role'];
+  protected $dates = ["deleted_at"];
+
+}
