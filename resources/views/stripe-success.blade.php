695bc1f3ae7d10f8148c898d96ab95427e7d3abf
antlogist <antlogist@gmail.com>
2021-12-11 21:09:59 +0300

    Success payment blade page created

---
+++ resources/views/stripe-success.blade.php
@@ -0,0 +1,19 @@
+@extends("layouts.app")
+
+@section("title", "Stripe Webhook")
+
+@section("data-page-id", "auth")
+
+@section("content")
+
+<div>
+<section>
+  <div class="row">
+    <div class="small-12 medium-7 medium-centered">
+        <h2 class="text-center">Thank you, we have received your payment and now processing your order.</h2>
+    </div>
+  </div>
+</section>
+</div>
+
+@stop
