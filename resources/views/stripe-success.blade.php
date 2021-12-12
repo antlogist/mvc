43821d133814beeb54b3e5f465fc5b0e6fb0ae40
antlogist <antlogist@gmail.com>
2021-12-12 11:14:12 +0300

    Markup fixed

--- resources/views/stripe-success.blade.php
+++ resources/views/stripe-success.blade.php
@@ -8,9 +8,11 @@
 
 <div>
 <section>
-  <div class="row">
-    <div class="small-12 medium-7 medium-centered">
-        <h2 class="text-center">Thank you, we have received your payment and now processing your order.</h2>
+  <div class="row" style="margin-top: 7rem;">
+    <div class="grid-container">
+      <div class="small-12 medium-7 medium-centered">
+          <h2 class="text-center">Thank you, we have received your payment and now processing your order.</h2>
+      </div>
     </div>
   </div>
 </section>
