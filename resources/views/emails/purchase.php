57521789942e2c211891af6c7639ad316b4545a6
antlogist <antlogist@gmail.com>
2021-12-14 22:11:06 +0300

    Purchase mail template created

---
+++ resources/views/emails/purchase.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<div style="width: 600px; margin: 0 auto; padding: 15px; bacground-color: white;">
+		<p>
+			Best regards, <br>
+			Anthony Underwood
+		</p>
+	</div>
+</body>
+</html>
