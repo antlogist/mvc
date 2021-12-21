bcd762a201799979b93d832b3a068eb591a3b560
antlogist <antlogist@gmail.com>
2021-12-21 10:41:34 +0300

    product, user, payment details added

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Carbon\Carbon;
 use App\Models\Product;
-use App\Models\SubCategory;
+use App\Models\Payment;
+use App\Models\User;
 
 class Order extends Model {
 
@@ -25,11 +26,14 @@ class Order extends Model {
         "user_id" => $item->user_id,
         "order_no" => $item->order_no,
         "product_id" => $item->product_id,
+        "product_name" => Product::where('id', $item->product_id)->first()->name,
         "quantity" => $item->quantity,
         "unit_price" => $item->unit_price,
         "status" => $item->status,
         "total" => $item->total,
-        "added" => $added->toFormattedDateString()
+        "added" => $added->toFormattedDateString(),
+        "payment_status" => Payment::where('order_no', $item->order_no)->first()->status,
+        "user_name" => User::where('id', $item->user_id)->first()->fullname
       ]);
     }
     return $orders;
