2eeb56a814afd1d30c6d3ea5dce1b05f478ff8b3
antlogist <antlogist@gmail.com>
2021-12-21 13:28:40 +0300

    Transform function added

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Carbon\Carbon;
 use App\Models\Product;
 use App\Models\SubCategory;
 
@@ -14,5 +15,22 @@ class Payment extends Model {
   public $timestamps = true;
   protected $fillable = ['user_id', 'order_no', 'amount', 'status'];
   protected $dates = ["deleted_at"];
+
+  function transform($data) {
+    $payments = [];
+    foreach($data as $item) {
+      $added = new Carbon($item->created_at);
+      array_push($payments, [
+        "id" => $item->id,
+        "user_id" => $item->user_id,
+        "order_no" => $item->order_no,
+        "amount" => $item->amount,
+        "status" => $item->status,
+        "user_name" => User::where('id', $item->user_id)->first()->fullname,
+        "added" => $added->toFormattedDateString()
+      ]);
+    }
+    return $payments;
+  }
 
 }
