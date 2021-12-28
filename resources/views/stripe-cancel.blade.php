82b094310b541c42ffe2a3394e9fa4b403f0fe48
antlogist <antlogist@gmail.com>
2021-12-28 15:10:01 +0300

    Paypal and stripe views added

---
+++ resources/views/stripe-cancel.blade.php
@@ -0,0 +1,21 @@
+@extends("layouts.app")
+
+@section("title", "Stripe Webhook")
+
+@section("data-page-id", "auth")
+
+@section("content")
+
+<div>
+<section>
+  <div class="row" style="margin-top: 7rem;">
+    <div class="grid-container">
+      <div class="small-12 medium-7 medium-centered">
+          <h2 class="text-center">Something went wrong</h2>
+      </div>
+    </div>
+  </div>
+</section>
+</div>
+
+@stop
